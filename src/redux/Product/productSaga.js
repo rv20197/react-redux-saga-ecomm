67bf11382f6d43c11872273bc8ccb9735c261a60
Vatsal Dinesh Rajgor <rv20197@gmail.com>
2022-06-23 11:02:46 +0530

    Configure Saga

---
+++ src/redux/Product/productSaga.js
@@ -0,0 +1,4 @@
+// Generator Function (It's used to handle async operations)
+function* productSaga() {}
+
+export default productSaga;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
+import createSagaMiddleware from 'redux-saga';
+
 import rootReducer from './rootReducer';
+import productSaga from './Product/productSaga';
 
-const store = configureStore({ reducer: rootReducer });
+const sagaMiddleware = createSagaMiddleware();
+const store = configureStore({
+	reducer: rootReducer,
+	middleware: () => [sagaMiddleware]
+});
 
+sagaMiddleware.run(productSaga);
 export default store;
