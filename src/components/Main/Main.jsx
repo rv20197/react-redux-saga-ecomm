62ab64548522231f368d0cad19adfdcb12c1246f
Vatsal Dinesh Rajgor <rv20197@gmail.com>
2022-06-26 19:03:09 +0530

    Product List UI with API data

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import {
@@ -8,39 +9,51 @@ import {
 } from '../../redux/Cart/cartAction';
 import { productListing } from '../../redux/Product/productAction';
 
+import './Main.css';
+
 const Main = () => {
 	const dispatch = useDispatch();
 
-	const cartData = useSelector(state => state.cartData);
-
 	const productData = useSelector(state => state.productData);
-	console.log('Data in main component from Saga: ', productData);
-	const product = {
-		name: 'iPhone 13',
-		type: 'Mobile',
-		color: 'Black',
-		price: 65000
-	};
 
-	const addToCartHandler = () => dispatch(addToCart(product));
-	const removeFromCartHandler = () => dispatch(removeFromCart(product.name));
+	const productListingHandler = () => dispatch(productListing());
+	const addToCartHandler = product => dispatch(addToCart(product));
+	const removeFromCartHandler = product => dispatch(removeFromCart(product.id));
 	const emptyCartHandler = () => dispatch(emptyCart());
 
-	const productListingHandler = () => dispatch(productListing('Hello'));
+	useEffect(() => {
+		productListingHandler();
+	}, []);
 
 	return (
 		<>
 			<div>
-				<button onClick={addToCartHandler}>Add to Cart</button>
-			</div>
-			<div>
-				<button onClick={removeFromCartHandler}>Remove from Cart</button>
-			</div>
-			<div>
 				<button onClick={emptyCartHandler}>Empty Cart</button>
 			</div>
-			<div>
-				<button onClick={productListingHandler}>Get Product List</button>
+			<div className='product-container'>
+				{productData.map(item => (
+					<div className='product-item' key={item.id}>
+						<img
+							src={item.image}
+							alt={item.productName}
+							width='200px'
+							height='200px'
+						/>
+						<div>Name: {item.productName}</div>
+						<div>Color: {item.color}</div>
+						<div>Price: {item.price}</div>
+						<div>Category: {item.category}</div>
+						<div>Brand: {item.brand}</div>
+						<div>
+							<button onClick={() => dispatch(addToCart(item))}>
+								Add to Cart
+							</button>
+							<button onClick={() => dispatch(removeFromCart(item.id))}>
+								Remove from Cart
+							</button>
+						</div>
+					</div>
+				))}
 			</div>
 		</>
 	);
